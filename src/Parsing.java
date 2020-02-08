721164196a4a62367c302c6ff7fcfd0944a6a02d
4rrak1s <nuastazi@gmail.com>
2020-02-08 20:04:24 +0200

    minor improvement

--- src/Parsing.java
+++ src/Parsing.java
@@ -6,9 +6,7 @@ public class Parsing {
     public static void main(String[] args) {
 
         String Sequence = "0832 973213 ada 074o-123 312 0721 912 123 123 321 212 0732-222111(7480)-732213 0932-213-231(0732)-1321220749 223-102a0792-010-110";
-        String Regex;
-
-        Regex = "\\(?07\\d{2}\\)?[-\\s]?\\d{3}[-\\s]?\\d{3}";
+        String Regex = "\\(?07\\d{2}\\)?[-\\s]?\\d{3}[-\\s]?\\d{3}";
 
         Pattern checkRegex = Pattern.compile(Regex);
         Matcher regexMatcher = checkRegex.matcher(Sequence);
